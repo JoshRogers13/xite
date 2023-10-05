81e09a8e63207023df1819de2cf191dd9c9c8535
JoshRogers13 <56076198+JoshRogers13@users.noreply.github.com>
2023-10-05 12:31:56 +0100

    Add js to the collection page to open and close the filters

---
+++ assets/collection.js
@@ -0,0 +1,35 @@
+var collectionFiltersWrapper = '.collection--filter-form-inner-wrapper',
+    collectionFiltersContainer = '.collection--filter-form-container',
+    collectionFiltersCloseBtn = '.execute--collection-filters-close',
+    collectionFilterTrigger = '#executeOpenCollectionFilters';
+
+// document.querySelector(collectionFiltersWrapper).classList.add('menu-is-open');
+
+// Function to open filter
+function openCollectionFilters() {
+    document.querySelector(collectionFiltersWrapper).classList.add('menu-is-open');
+}
+
+// Function to close filter
+function closeCollectionFilters() {
+    document.querySelector(collectionFiltersWrapper).classList.remove('menu-is-open');
+}
+
+document.querySelector(collectionFilterTrigger).addEventListener('click', (e) => {
+    openCollectionFilters();
+});
+
+// Close the filters when the overlay is clicked
+document.querySelector(collectionFiltersWrapper).addEventListener('click', () => {
+    closeCollectionFilters();
+});
+
+// Prevent interaction with the filters from closing the filters
+document.querySelector(collectionFiltersContainer).addEventListener('click', (e) => {
+    e.stopPropagation();
+});
+
+document.querySelector(collectionFiltersCloseBtn).addEventListener('click', (e) => {
+    e.preventDefault();
+    closeCollectionFilters();
+});
